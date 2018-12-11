09e4e088f3757a07a326ca2e304b7218f3da5fb6
Matthew Hatcher <matthewh@halfpetal.com>
2018-12-10 23:00:59 -0500

    Removing useless stuff.

--- routes/api.php
+++ routes/api.php
@@ -5,21 +5,6 @@ use PaladinsNinja\Models\Player;
 use PaladinsNinja\Models\Match;
 use PaladinsNinja\Models\Champion;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::get('champion/{champion}', function(Reqest $request, $champion) {
     return Champion::firstOrFail(['champion_id' => $champion]);
 });
