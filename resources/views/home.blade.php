c15b08fca743a6cfab669feee564eb7a2f1d4794
MatthewSH <matthewh@halfpetal.com>
2018-12-16 04:36:39 -0500

    Start of a new homepage design.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,42 +1,75 @@
 @extends('layouts.app')
 
+@section('head')
+<style>
+    body {
+        background: url('{{ asset('images/media/home/' . rand(1, 15) . '.png') }}');
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }
+
+    .container-fluid {
+        padding-right: 0px;
+        padding-left: 0px;
+    }
+</style>
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-12">
-            @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @endif
-            <form class="" role="form" action="{{ route('search') }}" method="POST">
-                {{  csrf_field() }}
-                <div class="card-body row no-gutters align-items-center">
-                    <div class="col">
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                {{--<button class="btn btn-outline-secondary dropdown-toggle rounded-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Platform</button>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" onclick="$('#platform').val('nintendo'); $(this).siblings('a').removeClass('active'); $(this).addClass('active');" href="#">Nintendo</a>
-                                    <a class="dropdown-item active" onclick="$('#platform').val('pc'); $(this).siblings('a').removeClass('active'); $(this).addClass('active');" href="#">PC</a>
-                                    <a class="dropdown-item" onclick="$('#platform').val('ps'); $(this).siblings('a').removeClass('active'); $(this).addClass('active');" href="#">PlayStation</a>
-                                    <a class="dropdown-item" onclick="$('#platform').val('xbl'); $(this).siblings('a').removeClass('active'); $(this).addClass('active');" href="#">Xbox</a>
-                                </div>--}}
-                            </div>
-                            <input name="platform" id="platform" type="hidden" value="pc">
-                            <input class="form-control form-control-lg rounded-0" type="text" id="name" name="name" placeholder="Find a Player">
-                        </div>
-                    </div>
-                    <div class="col-auto">
-                        <button class="btn btn-lg btn-success rounded-0" type="submit">Search</button>
-                    </div>
-                </div>
-            </form>
+<div class="container-fluid">
+    <section class="pb-5 pt-3 text-center text-white">
+        <h1>
+            Paladins Ninja
+        </h1>
+        <h4>Specially designed for Paladins players.<br>Built on community feedback and needs.</h4>
+
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
-    </div>
+        @endif
+
+        <form role="form" action="{{ route('search') }}" method="POST">
+            <div class="row no-gutters w-50 mt-5" style="margin-left:25%">
+                {{ csrf_field() }}
+                <div class="col">
+                    <input name="platform" id="platform" type="hidden" value="pc">
+                    <input class="form-control form-control-lg rounded-0" type="text" id="name" name="name" placeholder="Find a Player">
+                </div>
+
+                <div class="col-auto">
+                    <button class="btn btn-lg btn-success rounded-0" type="submit">Search</button>
+                </div>
+            </div>
+        </form>
+    </section>
+
+    <section class="pt-5">
+        <div class="card text-white bg-dark mb-3">
+            <div class="card-body d-flex justify-content-around text-center">
+                <div class="col">
+                    <h1>{{ \Cache::remember('site.totalmatches', 30, function() { return \PaladinsNinja\Models\Match::count(); }) }}+</h1>
+                    <h5 class="text-muted">MATCHES STORED</h5>
+                </div>
+
+                <div class="col">
+                    <h1>{{ \Cache::remember('site.totalplayers', 30, function() { return \PaladinsNinja\Models\Player::count(); }) }}+</h1>
+                    <h5 class="text-muted">PLAYERS STORED</h5>
+                </div>
+
+                <div class="col">
+                    <h1>{{ \Cache::remember('site.totalmatchestoday', 30, function() { return \PaladinsNinja\Models\Match::where('created_at', \Carbon\Carbon::today())->count(); }) }}+</h1>
+                    <h5 class="text-muted">MATCHES TODAY</h5>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
 </div>
 @endsection
