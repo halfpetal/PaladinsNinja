1ac66245b47f03e20bb2210cef146a9578fea96f
MatthewSH <matthewh@halfpetal.com>
2018-12-23 01:17:12 -0500

    Adding theme switcher.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,10 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    @if(session('user.theme') !== null)
+    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.3/{{ session('user.theme') }}/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    @endif
+
     <link rel="stylesheet" href="https://cdn.rawgit.com/tarkhov/postboot/v1.0.0-beta1/dist/css/postboot.min.css"/>
     <script src="https://cdn.rawgit.com/tarkhov/postboot/v1.0.0-beta1/dist/js/postboot.min.js"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.6.0/js/all.js" integrity="sha384-z9ZOvGHHo21RqN5De4rfJMoAxYpaVoiYhuJXPyVmSs8yn20IE3PmBM534CffwSJI" crossorigin="anonymous"></script>
@@ -49,79 +53,118 @@
 </head>
 <body>
     <div id="app" onload="loaded">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+            
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <form class="form-inline" role="form" action="{{ route('search') }}" method="POST">
-                            {{  csrf_field() }}
-                            <div class="row no-gutters">
-                                <input name="platform" id="platform" type="hidden" value="pc">
-                                <div class="col">
-                                    <input class="form-control form-control rounded-0" type="text" id="name" name="name" placeholder="Find a Player">
-                                </div>
-                                <div class="col-auto">
-                                    <button class="btn btn-success rounded-0" type="submit">Search</button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                    <form class="form-inline" role="form" action="{{ route('search') }}" method="POST">
+                        {{  csrf_field() }}
+                        <div class="row no-gutters">
+                            <input name="platform" id="platform" type="hidden" value="pc">
+                            <div class="col">
+                                <input class="form-control form-control rounded-0" type="text" id="name" name="name" placeholder="Find a Player">
+                            </div>
+                            <div class="col-auto">
+                                <button class="btn btn-success rounded-0" type="submit">Search</button>
+                            </div>
+                        </div>
+                    </form>
+                </ul>
+                
+                 <!-- Right Side Of Navbar -->
+                 <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('champions') }}">Champion List</a>
+                    </li>
+
+                    <li class="nav-item divider-vertical"></li>
+
+                    <li class="nav-item">
+                        <a class="nav-link navbar-brand" href="https://discord.gg/C6zQ6Yj"><i class="fab fa-discord"></i></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link navbar-brand" href="https://twitter.com/Paladins_Ninja"><i class="fab fa-twitter"></i></a>
+                    </li>
+
+                    <li class="nav-item divider-vertical"></li>
+
+                    <li class="nav-item">
+                        <a class="nav-link navbar-brand" href="#" data-toggle="modal" data-target="#siteThemeSwitcher"><i data-toggle="tooltip" data-placement="bottom" title="Change to a new theme." class="fa fa-tint"></i></a>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="siteThemeSwitcher" tabindex="-1" role="dialog" aria-labelledby="siteThemeSwitcherLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="siteThemeSwitcherLabel">Change Site Theme</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+
+                                    <div class="modal-body row mr-3 text-center">
+                                        <form class="col-6 mb-3" method="POST" action="{{ route('theme.remove') }}">
+                                            {{ method_field('DELETE') }}
+                                            {{ csrf_field() }}
+                                            <button class="btn btn-outline-primary" type="submit">
+                                                Remove Custom Theme
+                                            </button>
+                                        </form>
+
+                                        @foreach (['cerulean', 'cosmo', 'cyborg', 'darkly', 'flatly', 'journal', 'litera', 'lumen', 'lux', 'materia', 'minty', 'pulse', 'sandstone', 'simplex', 'sketchy', 'slate', 'solar', 'spacelab', 'superhero', 'united', 'yeti'] as $theme)
+                                        <form class="col-6 mb-3" method="POST" action="{{ route('theme.change', ['theme' => $theme]) }}">
+                                            {{ csrf_field() }}
+                                            <button class="btn btn-outline-primary" type="submit">
+                                                Switch to {{ title_case($theme) }}
+                                            </button>
+                                        </form>
+                                        @endforeach
+                                    </div>
                                 </div>
                             </div>
-                        </form>
-                    </ul>
+                        </div>
+                    </li>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+
+                {{-- <!-- Authentication Links -->
+                    @guest
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('champions') }}">Champion List</a>
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
-
-                        <li class="nav-item divider-vertical"></li>
-
-                        <li class="nav-item">
-                            <a class="nav-link navbar-brand" href="https://discord.gg/C6zQ6Yj"><i class="fab fa-discord"></i></a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link navbar-brand" href="https://twitter.com/Paladins_Ninja"><i class="fab fa-twitter"></i></a>
-                        </li>
-
-                    {{-- <!-- Authentication Links -->
-                        @guest
+                        @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest --}}
-                    </ul>
-                </div>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest --}}
+                </ul>
             </div>
         </nav>
 
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@ Route::get('champion/{champion}', 'HomeController@getChampion')->name('champion'
 Route::get('champions', 'HomeController@getAllChampions')->name('champions');
 Route::get('match/{match}', 'HomeController@getMatch')->name('match');
 Route::get('home', 'HomeController@index')->name('home');
+
+Route::post('theme/{theme}', function(\Illuminate\Http\Request $request, $theme) {
+    $themes = ['cerulean', 'cosmo', 'cyborg', 'darkly', 'flatly', 'journal', 'litera', 'lumen', 'lux', 'materia', 'minty', 'pulse', 'sandstone', 'simplex', 'sketchy', 'slate', 'solar', 'spacelab', 'superhero', 'united', 'yeti'];
+
+    if (in_array($theme, $themes)) {
+        $request->session()->put('user.theme', $theme);
+        return redirect()->back();
+    } else {
+        return abort(404);
+    }
+})->name('theme.change');
+
+Route::delete('theme', function(Request $request) {
+    session()->forget('user.theme');
+
+    return redirect()->back();
+})->name('theme.remove');
