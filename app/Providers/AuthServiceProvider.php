59f8c7becce1a77d652f1d1f061e12f2274fa6ea
MatthewSH <matthewh@halfpetal.com>
2018-12-23 01:16:59 -0500

    Removing Passport routes, for now.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,6 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        \Laravel\Passport\Passport::routes();
+        // \Laravel\Passport\Passport::routes();
     }
 }
