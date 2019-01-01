805ccda2d4ee80de0559b94da16e71973e9a8e45
MatthewSH <matthewh@halfpetal.com>
2019-01-01 00:32:19 -0500

    Adding margins.

--- resources/views/search-results.blade.php
+++ resources/views/search-results.blade.php
@@ -8,7 +8,7 @@
 
     @forelse($players as $p)
     @if($p['player_id'] != 0)
-    <a href="{{ route('player', ['player' => $p['player_id']]) }}" class="btn btn-primary btn-lg">{{ $p['portal'] }}</a>
+    <a href="{{ route('player', ['player' => $p['player_id']]) }}" class="m-2 btn btn-primary btn-lg">{{ $p['portal'] }}</a>
     @endif
     @empty
     <h5>No users with that name are found.</h5>
