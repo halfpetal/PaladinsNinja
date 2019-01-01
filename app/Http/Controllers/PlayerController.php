e68ef2883b5a5c7a5fe7fa19a7b9c3bbb5c15449
MatthewSH <matthewh@halfpetal.com>
2018-12-31 21:53:43 -0500

    Adding support for Switch and Xbox.

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -11,7 +11,23 @@ class PlayerController extends Controller
     public function search(SearchPlayer $request)
     {
         $players = \Cache::remember("search.name.{$request->name}", now()->addHours(6), function() use ($request) {
-            return resolve('PaladinsAPI')->getPlayerIdByName($request->name);
+            $players = resolve('PaladinsAPI')->getPlayerIdByName($request->name);
+            $playersXbox = resolve('PaladinsAPI')->getPlayerIdsByGamertag($request->name, 10);
+            $playersSwitch = resolve('PaladinsAPI')->getPlayerIdsByGamertag($request->name, 22);
+
+            if (!empty($playersXbox)) {
+                foreach($playersXbox as $px) {
+                    array_push($players, $px);
+                }
+            }
+
+            if (!empty($playersSwitch)) {
+                foreach($playersSwitch as $ps) {
+                    array_push($players, $ps);
+                }
+            }
+
+            return $players;
         });
 
         return view('search-results', [
