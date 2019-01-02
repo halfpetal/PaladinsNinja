8603bcf3164bbfab1248a7b884feeabbabd8fca4
MatthewSH <matthewh@halfpetal.com>
2019-01-02 12:45:54 -0500

    Fixing 404 error.

--- routes/API/v1/Champion.php
+++ routes/API/v1/Champion.php
@@ -1,7 +1,6 @@
 <?php
 
-Route::get('{champion}', 'ChampionController@show')->name('show');
 Route::get('list', 'ChampionController@index')->name('index');
-
+Route::get('{champion}', 'ChampionController@show')->name('show');
 Route::get('{champion}/skins', 'ChampionController@skinIndex')->name('skin.index');
 Route::get('{champion}/skins/{skin}', 'ChampionController@skinShow')->name('skin.show');
