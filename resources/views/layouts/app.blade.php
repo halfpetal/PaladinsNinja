b214615fa07731e322a1176140eaffebb975ea88
MatthewSH <matthewh@halfpetal.com>
2019-01-12 13:04:53 -0500

    Removing the ads, my bad.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,13 @@
     gtag('config', 'UA-91457849-4');
     </script>
 
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    {{--<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({
         google_ad_client: "ca-pub-6691059885236141",
         enable_page_level_ads: true
         });
-    </script>
+    </script>--}}
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
