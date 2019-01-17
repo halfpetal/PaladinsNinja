3a49e9c59eff310f2b4339db37adce53807fef84
MatthewSH <matthewh@halfpetal.com>
2019-01-16 22:32:57 -0500

    Just getting out of changes.

--- resources/js/components/paladins-news/PaladinsNewsHome.vue
+++ resources/js/components/paladins-news/PaladinsNewsHome.vue
@@ -1,6 +1,22 @@
 <template>
     <div>
-        <div v-if="newsItems.length > 0"></div>
+        <div v-if="newsItems.length > 0" class="row">
+            <div v-for="item in newsItems" :key="item.id" class="col-6 card">
+                <div class="card-body">
+                    <h5 class="card-title">{{ review.title }}</h5>
+                    <hr>
+                    <blockquote class="blockquote card-text">
+                        <p class="mb-0">
+                            {{ review.body }}
+                        </p>
+                        <footer class="blockquote-footer">
+                            {{ review.user.username }} | {{ review.rating }} <i class="fas fa-star"></i> | <a :href="`/match/${review.match_id}`">View Match Details</a>
+                        </footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+
         <h4 v-else>No news items, or could not retrieve them.</h4>
     </div>
 </template>
