dde02410fbd60eed13612624d1df8ff7bfbc04c4
MatthewSH <matthewh@halfpetal.com>
2019-01-17 11:26:06 -0500

    Adding player profile link to dropdown.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -108,9 +108,16 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('player', ['player' => Auth::user()->paladins_player_id]) }}">
+                                {{ __('My Player Profile') }}
+                            </a>
+
+                            <div class="dropdown-divider"></div>
+
                             <a class="dropdown-item" href="{{ route('settings') }}">
                                 {{ __('Settings') }}
                             </a>
+
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                     document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
