8b0c6be1cc8e5f7e7805339bb4243a39673483bc
MatthewSH <matthewh@halfpetal.com>
2019-01-31 14:34:15 -0500

    Removing the adsense auto ads.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,6 @@
     </script>
     @endif
 
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> <script> (adsbygoogle = window.adsbygoogle || []).push({ google_ad_client: "ca-pub-6691059885236141", enable_page_level_ads: true }); </script>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
