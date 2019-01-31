344d25234d5e9e95e3ec50e81541f79e70426542
MatthewSH <matthewh@halfpetal.com>
2019-01-31 14:25:04 -0500

    Adding new ad unit.

---
+++ resources/views/includes/ad.blade.php
@@ -0,0 +1,18 @@
+<div class="card border-dark">
+  <div class="card-header">Ad Unit</div>
+  <div class="card-body text-dark">
+    <div class="card-text">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Paladins Ninja -->
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-6691059885236141"
+            data-ad-slot="9931178183"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+        <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
+  </div>
+</div>
