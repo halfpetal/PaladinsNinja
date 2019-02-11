bf3f6c92c118c82cc272596235b7c8fa20023bf9
MatthewSH <matthewh@halfpetal.com>
2019-02-11 13:15:07 -0500

    Adding new method to match controller.

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -3,8 +3,12 @@
 namespace PaladinsNinja\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PaladinsNinja\Models\Match;
 
 class MatchController extends Controller
 {
-    //
+    public function show(Request $request, Match $match)
+    {
+        return view('match', ['match' => $match, 'tableView' => $request->get('tableView')]);
+    }
 }
